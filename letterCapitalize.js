f97689d142d9a002ee4eb80d5e09d2b7e1effa66
Michelle Sri <michelle.sri@gmail.com>
2017-07-26 14:36:17 -0700

    created initial variables for letterCapitalize

---
+++ letterCapitalize.js
@@ -0,0 +1,6 @@
+
+// Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space. 
+
+function LetterCapitalize(str) { 
+  var result = "";
+  var lastLetter = " ";
