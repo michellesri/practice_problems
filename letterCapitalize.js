13ad5639a59771f8207fa8cf26c0a8c57aa63cd1
Michelle Sri <michelle.sri@gmail.com>
2017-07-26 14:36:46 -0700

    finished letterCapitalize fn

--- letterCapitalize.js
+++ letterCapitalize.js
@@ -4,3 +4,23 @@
 function LetterCapitalize(str) { 
   var result = "";
   var lastLetter = " ";
+  
+  // loop through each letter
+  for (var i = 0; i < str.length; i++) {
+    // if first letter or if last letter was a space, add capitalized letter to result
+    if (lastLetter === " ") {
+      result += str[i].toUpperCase();
+    }
+    // else add current letter to result
+    else {
+      result += str[i];
+    }
+    
+    // set lastLetter to current letter for next loop run
+    lastLetter = str[i];
+  }
+
+  return result; 
+}
+
+//console.log(LetterCapitalize("This is the argument"));
