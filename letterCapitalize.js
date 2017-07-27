0c7297772c54e4f1312619e8aa16d73b34ba1450
Michelle Sri <michelle.sri@gmail.com>
2017-07-27 14:39:19 -0700

    started simpleSymbols fn

--- letterCapitalize.js
+++ letterCapitalize.js
@@ -1,5 +1,7 @@
 
-// Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space. 
+// Have the function LetterCapitalize(str) take the str parameter
+ // being passed and capitalize the first letter of each word. 
+ // Words will be separated by only one space. 
 
 function LetterCapitalize(str) { 
   var result = "";
