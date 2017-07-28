ac5084f79f2213675a8a5f95055ca24df0886e27
Michelle Sri <michelle.sri@gmail.com>
2017-07-28 16:46:42 -0700

    removed spaces from string for palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -4,3 +4,12 @@
 //     is backward) otherwise return the string false. For
 //     example: "racecar" is also "racecar" backwards.
 //      Punctuation and numbers will not be part of the string.
+
+function Palindrome(str) {
+  //create new string without spaces
+  var noSpaceString = "";
+    //loop through string
+  for (var i = 0; i < str.length; i++) {
+      // if not a space, then append to noSpaceString;
+    if (str[i] !== " ") {noSpaceString += str[i]};
+  }
