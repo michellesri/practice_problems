febdf22455da377b28111914b174f2f433b88694
Michelle Sri <michelle.sri@gmail.com>
2017-07-30 15:26:36 -0700

    incorrect solution. need to come back to this

--- arrayAddition1.js
+++ arrayAddition1.js
@@ -9,22 +9,36 @@
 
 function ArrayAdditionI(arr) {
   //base case
-  if (arr.length === 1) { return "true" }
-
-  //determine largest number and remove from array
-  var largest = arr.sort(function(a,b){return a-b}).pop();
-  //initialize sum variable
-  var sum = 0;
-
-  //for each number in the array in turn
-  for (var i = 0; i < arr.length; i++) {
-    //set sum to be equal to currently looped num
-    sum = arr[i];
-    // sum = addition of all other numbers in turn from left to right
-    for (var j = 0; j < arr.length; j++) {
-      if (j !== i) {
-        sum += arr[j];
-        if (sum === largest) {
-          return "true";
-        }
-      }
+//   if (arr.length === 1) { return "true" }
+//
+//   //determine largest number and remove from array
+//   var largest = arr.sort(function(a,b){return a-b}).pop();
+//   //initialize sum variable
+//   var sum = 0;
+//
+//   //for each number in the array in turn
+//   for (var i = 0; i < arr.length; i++) {
+//     //set sum to be equal to currently looped num
+//     sum = arr[i];
+//     // sum = addition of all other numbers in turn from left to right
+//     for (var j = 0; j < arr.length; j++) {
+//       if (j !== i) {
+//         sum += arr[j];
+//         if (sum === largest) {
+//           return "true";
+//         }
+//       }
+//       // sum = subtraction of all other numbers in turn from left to right
+//     for (var k = 0; k < arr.length; k++) {
+//       if (k != i) {
+//         sum -= arr[k];
+//         if (sum === largest) {
+//           return "true";
+//         }
+//       }
+//     }
+//
+//     //otherwise return "false"
+//     return "false";
+//   }
+// }
