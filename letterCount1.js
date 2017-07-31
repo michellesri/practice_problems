08b3cb3867f6704874cc141ded6da146c25e3c67
Michelle Sri <michelle.sri@gmail.com>
2017-07-31 12:55:24 -0700

    finished letterCount1.js

--- letterCount1.js
+++ letterCount1.js
@@ -7,32 +7,50 @@
 //   no words with repeating letters return -1. Words
 //    will be separated by spaces.
 
+
 function LetterCountI(str) {
- var maxRepeats = 1;
- var maxWord = "";
+  var maxRepeats = 1;
+  var maxWord = "";
 
- // split string into array of separate words
-  var wordArray = str.split(" ");
+// split string into array of separate words
+var wordArray = str.split(" ");
 
-  // loop through each word in array
-  for (var i = 0; i < wordArray.length; i++) {
-    var word = wordArray[i];
-    var maxRepeatsInWord = 0;
+// loop through each word in array
+for (var i = 0; i < wordArray.length; i++) {
+  var word = wordArray[i];
+  var maxRepeatsInWord = 0;
 
-    // loop through each letter of word
-    for (var j = 0; j < word.length; j++) {
-      var letter = word[j];
-      var letterRepeats = 1;
+  // loop through each letter of word
+  for (var j = 0; j < word.length; j++) {
+    var letter = word[j];
+    var letterRepeats = 1;
 
-      // loop through all other letters in word
-      for (var k = 0; k < word.length; k++) {
-        if (k != j) {
-          if (word[k] === letter) {
-            letterRepeats++;
-          }
+    // loop through all other letters in word
+    for (var k = 0; k < word.length; k++) {
+      if (k != j) {
+        if (word[k] === letter) {
+          letterRepeats++;
         }
       }
-      if (letterRepeats > maxRepeatsInWord) {
-        maxRepeatsInWord = letterRepeats;
-      }
+    }
+
+    if (letterRepeats > maxRepeatsInWord) {
+      maxRepeatsInWord = letterRepeats;
+    }
+  }
+
+  if (maxRepeatsInWord > maxRepeats) {
+    maxRepeats = maxRepeatsInWord;
+    maxWord = word;
+  }
 }
+
+  if (maxWord !== "") {
+    return maxWord;
+  } else {
+    return -1;
+  }
+
+}
+
+//console.log(LetterCountI("Today is the greatest day ever!"));
