d7111165c8b45ba849922cea37606102ee0f439a
Michelle Sri <michelle.sri@gmail.com>
2017-08-01 15:54:32 -0700

    split the string into two times

--- countMinutesI.js
+++ countMinutesI.js
@@ -1,1 +1,15 @@
-// Have the function CountingMinutesI(str) take the str parameter being passed which will be two times (each properly formatted with a colon and am or pm) separated by a hyphen and return the total number of minutes between the two times. The time will be in a 12 hour clock format. For example: if str is 9:00am-10:00am then the output should be 60. If str is 1:00pm-11:00am the output should be 1320.
+// Have the function CountingMinutesI(str)
+// take the str parameter being passed which will
+//  be two times (each properly formatted with a
+//     colon and am or pm) separated by a hyphen
+//      and return the total number of minutes
+//       between the two times. The time will be
+//        in a 12 hour clock format.
+//        For example: if str is 9:00am-10:00am then
+//        the output should be 60. If str is 1:00pm-11:00am
+//        the output should be 1320.
+
+function CountingMinutesI(str) {
+  //split string into array of two times, separated by the dash
+  var array = str.split("-");
+  // ["12:30pm", "12:00am"]
