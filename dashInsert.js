8798aec9b1a155acad3069641a24c70727c3335d
Michelle Sri <michelle.sri@gmail.com>
2017-08-04 19:28:52 -0700

    started dashInsert.js

---
+++ dashInsert.js
@@ -0,0 +1,3 @@
+//Have the function DashInsert(num) insert dashes ('-') between each
+ // two odd numbers in num. For example: if num is 454793 the output
+ //  should be 4547-9-3. Don't count zero as an odd number.
