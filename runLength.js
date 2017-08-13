63cae209adad392ba48051d8c910d8162c17eebd
Michelle Sri <michelle.sri@gmail.com>
2017-08-12 17:16:27 -0700

    changed initial variables for runLength

--- runLength.js
+++ runLength.js
@@ -1,5 +1,5 @@
 // run length (count the number of consecutive letters in a word)
 function RunLength(str) {
-  var newStr = "";
-  var letter = str[0];
-  var counter = 1;
+  var count = 1;
+  var result = "";
+}
