72d3c8056840795c124f4f757e4434d442c0f68b
Michelle Sri <michelle.sri@gmail.com>
2017-08-12 17:20:27 -0700

    build result string for count in runLength.js

--- runLength.js
+++ runLength.js
@@ -4,4 +4,9 @@ function RunLength(str) {
   var result = "";
   for (var i = 0; i < str.length; i++) {
         if (str[i] === str[i + 1]) {
+          count++
+        } else {
+            result += count + str[i];
+            count = 1;
+
 }
