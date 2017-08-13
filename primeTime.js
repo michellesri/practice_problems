09248bfdd6be23870546f1a9fdecd8446b04453e
Michelle Sri <michelle.sri@gmail.com>
2017-08-12 17:13:46 -0700

    finished primeTime.js

--- primeTime.js
+++ primeTime.js
@@ -5,3 +5,11 @@ function PrimeTime(num) {
   if (num < 2) return false;
   // if not an int
   if (num !== Math.ceil(num)) return false;
+  // check every whole number from 2 to the square root of n.
+  // If any of these divides n exactly, n cannot be prime.
+  for (var i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
