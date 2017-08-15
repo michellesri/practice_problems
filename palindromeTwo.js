4320c6982abffaec1a1bd536af1cc61ee2032214
Michelle Sri <michelle.sri@gmail.com>
2017-08-14 17:35:14 -0700

    created the forward string and reversed str

--- palindromeTwo.js
+++ palindromeTwo.js
@@ -1,1 +1,4 @@
 function PalindromeTwo(str) {
+  var strFor = str.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  var strRev =  str.replace(/[^a-zA-Z]/g, "").split("").reverse().join("").toLowerCase();
+  
