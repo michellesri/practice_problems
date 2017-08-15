19e32cd7fb82b4b27fd5a3cad652e21688691a71
Michelle Sri <michelle.sri@gmail.com>
2017-08-14 17:36:08 -0700

    added comments to palindromeTwo.js

--- palindromeTwo.js
+++ palindromeTwo.js
@@ -1,5 +1,7 @@
+// check if a string is a palindrome
+
 function PalindromeTwo(str) {
   var strFor = str.replace(/[^a-zA-Z]/g, "").toLowerCase();
   var strRev =  str.replace(/[^a-zA-Z]/g, "").split("").reverse().join("").toLowerCase();
   return strFor === strRev;
-}  
+}
