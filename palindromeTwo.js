76e8a2ee6a0718372f9e02c2b58ff7e4887df3cd
Michelle Sri <michelle.sri@gmail.com>
2017-08-14 17:35:34 -0700

    finished palindromeTwo.js

--- palindromeTwo.js
+++ palindromeTwo.js
@@ -1,4 +1,5 @@
 function PalindromeTwo(str) {
   var strFor = str.replace(/[^a-zA-Z]/g, "").toLowerCase();
   var strRev =  str.replace(/[^a-zA-Z]/g, "").split("").reverse().join("").toLowerCase();
-  
+  return strFor === strRev;
+}  
