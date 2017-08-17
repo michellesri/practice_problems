c7548829e10aaa9644c8a5b09a5d4c9bdcbc5d6a
Michelle Sri <michelle.sri@gmail.com>
2017-08-16 17:42:01 -0700

    finished division.js

--- division.js
+++ division.js
@@ -3,3 +3,9 @@ function Division(num1, num2) {
     var smallerNum = min(num1, num2)
 
     for (i = 1; i <= smallerNum; i++) {
+      if ((num1 % i === 0) && (num2 % i === 0) && (i > largestFactor)) {
+          largestFactor = i;
+      }
+    }
+    return largestFactor;
+}
