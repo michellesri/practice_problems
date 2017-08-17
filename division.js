4034918ae857e1c989bd6570de9ad58eb1a0a165
Michelle Sri <michelle.sri@gmail.com>
2017-08-16 17:39:44 -0700

    refactored smallerNum to use min()

--- division.js
+++ division.js
@@ -1,8 +1,3 @@
 function Division(num1, num2) {
     var largestFactor = 1;
-    var smallerNum;
-    if (num1 > num2) {
-      smallerNum = num2;
-  } else {
-      smallerNum = num1;
-  }
+    var smallerNum = min(num1, num2)
